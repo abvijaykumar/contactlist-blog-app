238e15c2a639d88b06715c7a2dbfaf504dcd07d9
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2021-12-01 13:33:23 +0530

    removed Appstop

--- cd-scripts/setup.sh
+++ cd-scripts/setup.sh
@@ -4,7 +4,7 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
-sudo chmod +x scripts/appStop.sh
+sudo chmod +x ./appStop.sh
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/contacts-app"
